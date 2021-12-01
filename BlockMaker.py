d6cba88a9e592281a6679797666e3610b7d011b8
teodoraruvceski <teodoraruvceski@gmail.com>
2021-12-01 20:58:15 +0100

    blocking
    radi u blokirajucem rezimu

--- BlockMaker.py
+++ BlockMaker.py
@@ -12,25 +12,45 @@ class BlockMaker:
         self.ipAddr=socket.gethostbyname(socket.gethostname())
         
     def recieveTransactions(self):
-        HOST=self.ipAddr
+        HOST=''
         PORT=5000
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.setblocking(0)
+        #s.setblocking(0)
         s.bind((HOST,PORT))
         print('bindovao')
         s.listen(10)
         inputs = [s]
         outputs = []
-        ##connection,addr=s.accept()
+        #connection,addr=s.accept()
+        # while True:
+        #   print('pocetak whilea')
+        #   readable, writable, exceptional = select.select(inputs,outputs,inputs)
+        #   for ss in readable:
+        #        if ss is not s:
+        #            print(readable.count()) 
+        #            ##obj=json.loads(json.parse(s.recv(1024)))
+        #            ## data = Transaction(**obj)
+        #            data=s.recv(1024)
+        #            if data:
+        #             print(data)
         while True:
-          print('pocetak whilea')
-          readable, writable, exceptional = select.select(inputs,outputs,inputs)
-          for ss in readable:
-               if ss is not s:
-                   print(readable.count()) 
-                   ##obj=json.loads(json.parse(s.recv(1024)))
-                   ## data = Transaction(**obj)
-                   data=s.recv(1024)
-                   if data:
-                    print(data)
-                
+        # Wait for a connection
+            print('waiting for a connection') 
+            connection, client_address = s.accept()       
+            try:
+                print('connection from', client_address)
+
+                # Receive the data in small chunks and retransmit it
+                while True:
+                    data = connection.recv(1024)
+                    print('received "%s"' % pickle.loads(data))
+                    if data:
+                        print ('sending data back to the client')
+                        #connection.sendall(data)
+                    else:
+                        #print >>sys.stderr, 'no more data from', client_address
+                        break
+                    
+            finally:
+                # Clean up the connection
+                connection.close()
--- Vallet.py
+++ Vallet.py
@@ -19,7 +19,7 @@ class Vallet:
         TCP_PORT = 5000
         BUFFER_SIZE = 1024
        ## MESSAGE = pickle.dumps(transaction)
-        MESSAGE = 'message'##json.dumps(transaction)
+        MESSAGE = pickle.dumps('message')##json.dumps(transaction)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((TCP_IP, TCP_PORT))
         s.send(MESSAGE)
