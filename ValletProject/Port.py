868673e515fb8effa9ad59a35916e8f0bd95e068
gordiic <gordiic@users.noreply.github.com>
2021-12-03 12:56:29 +0100

    commitN2

---
+++ ValletProject/Port.py
@@ -0,0 +1,12 @@
+
+
+# class Port:
+#     @staticmethod
+#     def GetPort():
+#         with open('readme.txt','r') as f:
+#             lines = f.readlines()       
+#         f.Close()
+#         FindPort(lines)
+        
+#     def FindPort(self,ports):
+        
--- ValletProject/Vallet.py
+++ ValletProject/Vallet.py
@@ -5,6 +5,10 @@ import time
 import json
 import sys
 import select
+import keyboard
+#import portalocker
+
+
 class Vallet:
     counter=0
     def __init__(self):
@@ -14,6 +18,39 @@ class Vallet:
         self.transactions=[]
         self.ipAddr=socket.gethostbyname(socket.gethostname())
         
+    def GetPort(self):
+        with open('portconfig.txt','r') as f:
+             lines = f.readlines()
+             
+        print(lines)
+        array = lines[0].split(',')
+        print(array)
+        max=5001
+        idx=0
+        for port in array:
+            idx+=1
+            if int(port)>max:
+                if int(port)>(max+1): #5001,5002,5003,5004
+                    #max=max+1
+                    break
+                max = int(port)
+        max+=1
+        array.insert(idx,max)
+        text=""
+        for port in array:
+            text=text+str(port)+","
+        
+        textlist = list(text)
+        #print("TEXTLIST:",textlist)
+        textlist.pop()
+        #print("TEXTLIST2:",textlist)
+        text2= "".join(textlist)
+        print(text2)
+        with open('portconfig.txt', 'w') as f:
+             f.write(text2)
+        f.close()
+        return max
+
     def CreateTransaction(self,sum,receiver):
         transaction=Transaction.Transaction(sum,self.ipAddr,receiver,self.balance,time.time())
         TCP_IP = '127.0.0.1'
@@ -29,13 +66,14 @@ class Vallet:
         
     def ReceiveMoney(self):
         HOST=''
-        PORT=5001
+        PORT=5001 #self.GetPort()
+        print(PORT)
         ss=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         ss.bind((HOST,PORT))
         print('bindovao')
         ss.listen(5)
-        print ("Listening on port 5001")
+        print ("Listening on port",PORT)
         read_list = [ss]
         while True:
             readable, writable, errored = select.select(read_list, [], [])
